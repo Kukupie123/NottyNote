d8c00b40c99c39d819d2a8fc9be4b8f8cbb2034d
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2022-10-21 07:46:30 +0530

    Fixed it by using the correct instruction provided in git of jwt

--- gateway/src/main/java/kuku/advbkm/gateway/service/JWTService.java
+++ gateway/src/main/java/kuku/advbkm/gateway/service/JWTService.java
@@ -1,9 +1,15 @@
 package kuku.advbkm.gateway.service;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -11,12 +17,12 @@ import java.time.temporal.ChronoUnit;
 @Service
 public class JWTService {
 
-    private String key;
-    private JwtParser parser;
+    final private SecretKey key;
+    final private JwtParser parser;
 
     public JWTService() {
-        key = "1234567890";
-        parser = Jwts.parser();
+        key = Keys.hmacShaKeyFor("Keys.hmacShaKeyFor(\"1234567890\".getBytes(StandardCharsets.UTF_8));".getBytes(StandardCharsets.UTF_8));
+        parser = Jwts.parserBuilder().setSigningKey(key).build();
     }
 
 
@@ -25,19 +31,16 @@ public class JWTService {
                 .setSubject(userName)
                 .setIssuedAt(Date.from(Instant.now()))
                 .setExpiration(Date.from(Instant.now().plus(15, ChronoUnit.MINUTES)))
-                .signWith(SignatureAlgorithm.HS256,key);
+                .signWith(key);
 
 
-        String token = builder.compact();
-
-        return token;
+        return builder.compact();
 
     }
 
     public String getUserName(String token) {
         //Subject is where we store the username
         return parser
-                .setSigningKey(key)
                 .parseClaimsJws(token)
                 .getBody().getSubject();
 
@@ -46,11 +49,10 @@ public class JWTService {
 
     public boolean isValid(String token, UserDetails user) {
         Claims claims = parser
-                .setSigningKey(key)
                 .parseClaimsJws(token).
                 getBody();
         boolean unexpired = claims.getExpiration().after(Date.from(Instant.now()));
 
-        return unexpired && user.getUsername() == claims.getSubject();
+        return unexpired && user.getUsername().equals(claims.getSubject());
     }
 }
