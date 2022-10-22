908e1bcbbc157534675a419ac4f808407957f8f7
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2022-10-22 20:09:18 +0530

    Updated self note.docx

--- gateway/src/main/java/kuku/advbkm/gateway/GatewayApplication.java
+++ gateway/src/main/java/kuku/advbkm/gateway/GatewayApplication.java
@@ -3,11 +3,15 @@ package kuku.advbkm.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+
+		
 	}
 
 }
