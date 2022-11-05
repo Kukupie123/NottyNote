fe6333d0caeceb82730c1b1b746669f32b9e37b9
kuchukpie@gmail.com <kuchukboromd@gmail.com>
2022-11-05 11:06:51 +0530

    Added vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <LoginPage c/>
+</template>
+
+<script>
+import LoginPage from "@/Page/LoginPage/LoginPage";
+
+export default {
+  name: 'App',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style>
+
+</style>
